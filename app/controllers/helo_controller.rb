1363f8969ae35a4a2c8f10f08ffef7b95cc474aa
田中敏之 <gakitherock19841102@gmail.com>
2019-02-13 23:42:30 +0900

    Section 2-4 フォームヘルパーを使おう
    複数洗濯可能なリスト

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -5,13 +5,16 @@ class HeloController < ApplicationController
       if request.post? then
         @title = 'Result'
         if params['s1'] then
-          @msg = 'you selected: ' + params['s1']
+          @msg = 'you selected: '
+          for val in params['s1']
+            @msg += val + ' '
+          end
         else
           @msg = 'not selected...'
         end
       else
         @title = 'Index'
-        @msg = 'select List...'
+        @msg = 'select radio List...'
       end
 
   end
